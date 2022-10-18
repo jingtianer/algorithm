0a7bbb70d5a0898a3afe4a93ad3c78fe6084751f
jingtianer <1043672911@qq.com>
2022-10-18 16:46:00 +0800

    2022年10月18日

---
+++ temp.cpp
@@ -0,0 +1,41 @@
+#include<vector>
+#include<map>
+#include<iostream>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+using namespace std;
+class Solution {
+public:
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        int bit = getBit(n);
+        int len = digits.size();
+        int count = len*(1-pow(len, bit-1))/(1-len);
+        return count;
+    }
+    int pow(int x, int n) {
+        int res = 1;
+        while(n) {
+            if(n&1){
+                res *= x;
+            }
+            n /= 2;
+            x *= x;
+        }
+        return res;
+    }
+    int getBit(int n) {
+        int count = 0;
+        while(n) {
+            count++;
+            n/=10;
+        }
+        return count;
+    }
+};
+int main() {
+    Solution s;
+    for(int i = 0; i < 100; i++)
+        cout << s.pow(2,i) << endl;
+    return 0;
+}
